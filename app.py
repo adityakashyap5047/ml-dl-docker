d136059d0de1ec8cd644940be7079aa5094bae3b
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-30 01:14:21 +0530

    Adding initial files

---
+++ app.py
@@ -0,0 +1,13 @@
+## Falsk app for hello world
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello World"
+
+if __name__=="__main__":
+    app.run(debug=True)
